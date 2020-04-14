c48a022029be06ac2f93520a6f0d0dd6a81125d8
Dimas186 <63162718+Dimas186@users.noreply.github.com>
2020-04-14 18:21:42 +0300

    ocr

--- app/src/main/java/com/example/intelligentsystem/MainActivity.java
+++ app/src/main/java/com/example/intelligentsystem/MainActivity.java
@@ -1,14 +1,90 @@
 package com.example.intelligentsystem;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.IOException;
+
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void onClick(View v) {
+        showImageImportDialog();
+    }
+
+    private void showImageImportDialog() {
+        Intent intent = new Intent(this, ImageSelectActivity.class);
+        intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_GALLERY, true);//default is true
+        startActivityForResult(intent, 1213);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == 1213) {
+                String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+                CropImage.activity(Uri.fromFile(new File(filePath)))
+                        .setGuidelines(CropImageView.Guidelines.ON).start(this);
+            }
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), resultUri);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                if (!recognizer.isOperational()) {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                } else {
+                    Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+                    SparseArray<TextBlock> items = recognizer.detect(frame);
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < items.size(); i++) {
+                        TextBlock myitem = items.valueAt(i);
+                        sb.append(myitem.getValue());
+                        sb.append("\n");
+                    }
+                    textView.setText(sb.toString());
+                }
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                Toast.makeText(this, "" + error, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
